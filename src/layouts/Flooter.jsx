940309e4c1fe420c29ac05d8cf35f47113d1b2ba
Şükran Karakaya <sukran.karakaya@ogrenci.selcuk.edu.tr>
2021-07-05 08:41:13 +0300

    11.Gün 1.Ödev

--- src/layouts/Flooter.jsx
+++ src/layouts/Flooter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Icon, Container } from 'semantic-ui-react'
 import flooter from '../css/flooter.css'
-import { Image, List,  } from 'semantic-ui-react'
+import { Image, List, Segment } from 'semantic-ui-react'
 
 
 
@@ -25,45 +25,49 @@ export default function Flooter() {
 
 
             <div className="help">
-                <h1><p><i>Yardım Al </i></p></h1>
-                <br />
+                <div className="floot1">
+                    <List>
+                        <List.Item><p>HAKKIMIZDA</p></List.Item>
+                        <List.Item><p>Hakkımızda</p></List.Item>
+                        <List.Item><p>Reklam Verin</p></List.Item>
+                        <List.Item><p>İletişim</p></List.Item>
+                        <List.Item><p>Kariyer Rehberi</p></List.Item>
+                        <List.Item><p>İlan Satın Al</p></List.Item>
+                        <List.Item><p>İK Blog</p></List.Item>
+                    </List>
 
-                <List animated verticalAlign='middle'>
-                    <List.Item>
-                        <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
-                        <List.Content>
-                            <List.Header><p>Handel</p></List.Header>
-                        </List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <Image avatar src='https://react.semantic-ui.com/images/avatar/small/christian.jpg' />
-                        <List.Content>
-                            <List.Header><p>Christian</p></List.Header>
-                        </List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
-                        <List.Content>
-                            <List.Header><p>Daniel</p></List.Header>
-                        </List.Content>
-                    </List.Item>
-                </List>
+                </div>
 
 
 
+
+                <div className="floot2">
+                    <List>
+                        <List.Item><p> YASAL</p></List.Item>
+                        <List.Item><p>Kullanım Koşulları</p></List.Item>
+                        <List.Item><p>Gizlilik Sözleşmesi</p></List.Item>
+                        <List.Item><p>Üyelik Sözleşmesi</p></List.Item>
+                        <List.Item><p>Çerezlerin Kullanımı</p></List.Item>
+                        <List.Item><p>Kalite Politikası</p></List.Item>
+                        <List.Item><p>KVKK Metni</p></List.Item>
+                    </List>
+
+                </div>
+                <div className="floot3">
+                    <List>
+                        <List.Item><p> YASAL</p></List.Item>
+                        <List.Item><p>Kullanım Koşulları</p></List.Item>
+                        <List.Item><p>Gizlilik Sözleşmesi</p></List.Item>
+                        <List.Item><p>Üyelik Sözleşmesi</p></List.Item>
+                        <List.Item><p>Çerezlerin Kullanımı</p></List.Item>
+                        <List.Item><p>Kalite Politikası</p></List.Item>
+                        <List.Item><p>KVKK Metni</p></List.Item>
+                    </List>
+
+                </div>
             </div>
-            <div className="list">
-                <List>
-                    <List.Item><p>HAKKIMIZDA</p></List.Item>
-                    <List.Item><p>Hakkımızda</p></List.Item>
-                    <List.Item><p>Reklam Verin</p></List.Item>
-                    <List.Item><p>İletişim</p></List.Item>
-                    <List.Item><p>Kariyer Rehberi</p></List.Item>
-                    <List.Item><p>İlan Satın Al</p></List.Item>
-                    <List.Item><p>İK Blog</p></List.Item>
-                </List>
 
-            </div>
+
 
 
         </div>
